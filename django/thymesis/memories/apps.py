614ed6c0ff1b44765445372e3fe16b7c4b15325f
yasserkabbout <yasserkabbout@gmail.com>
2018-12-02 22:19:54 +0300

    added_memories_Basic_Api

---
+++ django/thymesis/memories/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MemoriesConfig(AppConfig):
+    name = 'memories'
--- django/thymesis/thymesis/settings.py
+++ django/thymesis/thymesis/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memories',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
