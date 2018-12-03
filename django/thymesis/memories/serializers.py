e6a39eafef66d2c361d56829ce10d55c2457f419
Mehmet Kayaalp <mehmet.kayaalp@ebrandvalue.com>
2018-12-03 21:18:23 +0300

    Some essential changes are done

--- django/thymesis/memories/serializers.py
+++ django/thymesis/memories/serializers.py
@@ -4,13 +4,14 @@ from memories import models
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Users
-        fields = ('id', 'firstname', 'lastname', 'user_email', 'user_password','mobile_number','is_active')
+        fields = ('id', 'firstname', 'lastname', 'user_email', 'username',
+                  'home_page', 'user_password', 'mobile_number', 'is_active')
 
 
 class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Posts
-        fields = ('id', 'user_id', 'uri', 'title', 'summary', 'post_body', 'post_datetime', 'votes')
+        fields = ('id', 'user_id', 'uri', 'location', 'title', 'summary', 'post_body', 'post_datetime', 'votes')
 
 
 class CommentsSerializer(serializers.ModelSerializer):
--- django/thymesis/memories/views.py
+++ django/thymesis/memories/views.py
@@ -4,6 +4,7 @@ from .models import Users, Posts, Comments
 from .serializers import UsersSerializer, PostsSerializer, CommentsSerializer
 from rest_framework.response import Response
 from rest_framework.views import status
+import requests
 
 
 # Create your views here.
@@ -16,9 +17,11 @@ class ListPostsView(generics.ListAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostsSerializer
 
+
 class ListCommentsView(generics.ListAPIView):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
+
 
 class ListCreatePostsView(generics.ListCreateAPIView):
     queryset = Posts.objects.all()
@@ -28,16 +31,18 @@ class ListCreatePostsView(generics.ListCreateAPIView):
         a_Post = Posts.objects.create(
             user_id=request.data["user_id"],
             title=request.data["title"],
+            location=request.data["location"],
             summary=request.data["summary"],
-            post_body = request.data["post_body"],
-            uri = request.data["uri"],
-            votes = request.data["votes"]
+            post_body=request.data["post_body"],
+            uri=request.data["uri"],
+            votes=request.data["votes"]
             
         )
         return Response(
             data=PostsSerializer(a_Post).data,
             status=status.HTTP_201_CREATED
         )
+
 
 class ListCreateUsersView(generics.ListCreateAPIView):
     queryset = Users.objects.all()
@@ -48,11 +53,23 @@ class ListCreateUsersView(generics.ListCreateAPIView):
             firstname=request.data["firstname"],
             lastname=request.data["lastname"],
             user_email=request.data["user_email"],
-            user_password = request.data["user_password"],
-            mobile_number = request.data["mobile_number"],
-            is_active = request.data["is_active"]
+            user_password=request.data["user_password"],
+            mobile_number=request.data["mobile_number"],
+            is_active=request.data["is_active"],
+            username=request.data["username"],
+            home_page=request.data["home_page"],
             
         )
+
+        response = requests.put('http://127.0.0.1:5000/add/creator', data={'email': request.data["user_email"],
+                                                     'name': request.data["firstname"],
+                                                     'nick': request.data["username"],
+                                                     'home_page': request.data["home_page"],
+                                                     'type': 'person',
+                                                     'id': 89
+                                                     }
+                                 )
+
         return Response(
             data=UsersSerializer(a_User).data,
             status=status.HTTP_201_CREATED
