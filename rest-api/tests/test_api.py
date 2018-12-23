0e6886c9b066d5e9147914cde8cb6302afb30f3d
Mehmet Kayaalp <mehmet.kayaalp@ebrandvalue.com>
2018-12-24 01:53:47 +0300

    Major other creator endpoint is tested

--- rest-api/tests/test_api.py
+++ rest-api/tests/test_api.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import pytest
 from app import app
 import os
@@ -16,33 +17,102 @@ def client():
     os.unlink(app.config['DATABASE'])
 
 
-def test_check_available_url_annotation(client):
+def test_check_available_url_add_annotation(client):
     rv = client.get('/')
     assert b'/add/annotation' in rv.data
 
 
-def test_check_available_url_creator(client):
+def test_check_available_url_add_creator(client):
     rv = client.get('/')
     assert b'/add/creator' in rv.data
 
 
-def test_check_creator_cannot_post(client):
+def test_check_available_url_add_creator_id(client):
+    rv = client.get('/')
+    assert b'/get/creator/<id>' in rv.data
+
+
+def test_check_available_url_get_creator_list(client):
+    rv = client.get('/')
+    assert b'/get/creator/list' in rv.data
+
+
+def test_check_available_url_update_creator(client):
+    rv = client.get('/')
+    assert b'/update/creator' in rv.data
+
+
+def test_check_available_url_delete_annotation(client):
+    rv = client.get('/')
+    assert b'/delete/annotation/<id>' in rv.data
+
+
+def test_check_available_url_get_annotation(client):
+    rv = client.get('/')
+    assert b'/get/annotation/<id>' in rv.data
+
+
+def test_check_available_url_get_annotations(client):
+    rv = client.get('/')
+    assert b'/get/annotations/<ids>' in rv.data
+
+
+def test_check_available_url_get_annotation_by_target_id(client):
+    rv = client.get('/')
+    assert b'/get/annotation/target/<id>' in rv.data
+
+
+def test_check_add_creator_with_post(client):
     rv = client.post('/add/creator', json={
         'email': 'test@gmail.com'
     })
-    print(rv.data)
     assert b'405 Method Not Allowed' in rv.data
 
 
-def test_check_creator_put(client):
+def test_check_add_creator(client):
     rv = client.put('/add/creator', data=dict(
         email='test@gmail.com',
         home_page='home_page',
         id='5',
     ), follow_redirects=True)
+    assert b'Creator created successfully!' in rv.data
 
-    print(rv.data)
-    assert b'created_time":["This field is required."]' in rv.data
+
+def test_check_add_creator_without_mail(client):
+    rv = client.put('/add/creator', data=dict(
+        home_page='home_page',
+        id='5',
+    ), follow_redirects=True)
+    assert b'Ops, creator could not be created!' in rv.data
+
+
+def test_check_add_creator_without_id(client):
+    rv = client.put('/add/creator', data=dict(
+        email='test@gmail.com',
+        home_page='home_page',
+    ), follow_redirects=True)
+    assert b'Ops, creator could not be created!' in rv.data
+
+
+def test_check_add_creator_without_home_page(client):
+    rv = client.put('/add/creator', data=dict(
+        email='test@gmail.com',
+        id='5',
+    ), follow_redirects=True)
+    assert b'Ops, creator could not be created!' in rv.data
+
+
+def test_check_add_creator_already(client):
+    rv = client.put('/add/creator', data=dict(
+        email='test@gmail.com',
+        home_page='home_page',
+        id='5',
+    ), follow_redirects=True)
+    assert b'Ops, this email is already taken' in rv.data
+
+# Different options should be written like instead of writing email, write int.
+# Instead of writing int id, write string ...
+# get_spesific_creator_by_id methods should be written.
 
 
 def test_check_creator_get(client):
